d3a4f8654967758aa4f96682e525313b524226a0
benoit.duffez@gmail.com <benoit.duffez@gmail.com@5c0d1bba-9ab4-11de-979f-3176e26b92dd>
2010-01-19 21:47:00 +0000

    PVR: - Fixes issue 65

--- src/org/madprod/freeboxmobile/pvr/Disque.java
+++ src/org/madprod/freeboxmobile/pvr/Disque.java
@@ -8,13 +8,26 @@ import org.json.JSONObject;
  * @author bduffez
  *
  * Exemple:
+ * [
  * {
- * "free_size":3376040,
- * "total_size":41917188,
+ * "free_size":38375124,
+ * "total_size":38392640,
  * "label":"Disque dur",
  * "id":0,
  * "mount_point":"/Disque dur/Enregistrements"
+ * },
+ * {
+ * "nomedia":false,
+ * "dirty":false,
+ * "total_size":3656712,
+ * "readonly":false,
+ * "free_size":3656088,
+ * "label":"KEVIN",
+ * "id":2,
+ * "busy":false,
+ * "mount_point":"/KEVIN"
  * }
+ * ];
  *
  */
 public class Disque {
@@ -23,6 +36,10 @@ public class Disque {
 	private String mLabel;
 	private int mId;
 	private String mMountPt;
+	boolean mNoMedia;
+	boolean mDirty;
+	boolean mReadOnly;
+	boolean mBusy;
 	
 	public Disque(String json) {
 		try {
@@ -33,14 +50,39 @@ public class Disque {
 			this.mLabel = o.getString("label");
 			this.mId = o.getInt("id");
 			this.mMountPt = o.getString("mount_point");
+			
+			this.mNoMedia = getBoolean(o, "nomedia");
+			this.mDirty = getBoolean(o, "dirty");
+			this.mBusy = getBoolean(o, "busy");
+			this.mReadOnly = getBoolean(o, "readonly");
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
+	private boolean getBoolean(JSONObject o, String key) {
+		if (o.has(key)) {
+			try {
+				return o.getBoolean(key);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return false;
+	}
+	
 	public String getMountPt() {
 		return this.mMountPt;
+	}
+	
+	public String getLabel() {
+		return this.mLabel;
+	}
+	
+	public boolean isOk() {
+		return mDirty == false && mBusy == false && mReadOnly == false && mNoMedia == false;
 	}
 	
 	public int getId() {
