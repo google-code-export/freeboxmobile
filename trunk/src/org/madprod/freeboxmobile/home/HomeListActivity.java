4d7771cbdd3a90cd97bc0871f48231b68642715d
olivier.rosello34 <olivier.rosello34@5c0d1bba-9ab4-11de-979f-3176e26b92dd>
2010-02-11 12:45:12 +0000

    HOME : Ajout icône GuideTV

--- trunk/src/org/madprod/freeboxmobile/home/HomeListActivity.java
+++ trunk/src/org/madprod/freeboxmobile/home/HomeListActivity.java
@@ -197,7 +197,7 @@ public class HomeListActivity extends ListActivity implements HomeConstants
 		map.put(M_CLASS, org.madprod.freeboxmobile.ligne.LigneInfoActivity.class);
 		modulesList.add(map);
 		map = new HashMap<String,Object>();
-		map.put(M_ICON, R.drawable.icon_fbm);
+		map.put(M_ICON, R.drawable.fm_guide_tv);
 		map.put(M_TITRE, getString(R.string.buttonGuide));
 		map.put(M_DESC, "Consultez le guide TV, programmez des enregistrements, regardez la télévision\n\nCette fonctionnalité n'est pas encore disponible");
 		map.put(M_CLASS, null);
