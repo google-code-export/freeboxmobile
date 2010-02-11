be35681b82e27250dabfe1db8182dd2e8ea69747
olivier.rosello34 <olivier.rosello34@5c0d1bba-9ab4-11de-979f-3176e26b92dd>
2010-02-11 18:45:02 +0000

    Chargement de la liste des boitiers et des disques avec la liste des chaines. Boitiers et disques ds la BDD

--- trunk/src/org/madprod/freeboxmobile/pvr/PvrNetwork.java
+++ trunk/src/org/madprod/freeboxmobile/pvr/PvrNetwork.java
@@ -61,75 +61,119 @@ public class PvrNetwork extends AsyncTask<Void, Integer, Boolean> implements Con
      * @return true en cas de succès, false sinon
      */
     private boolean telechargerEtParser() {
-        // Récupérer chaines et disques durs        
+    	int boitier = 0;
+    	int nbBoitiers = 0;
+    	int bNum = 0;
+    	boolean ok = true;
+    	List<String> mBoitiersName;
+    	List<Integer> mBoitiersNb;
+    	
+        // Récupérer chaines et disques durs et boitiers
         String url = "http://adsl.free.fr/admin/magneto.pl";
-        List<NameValuePair> param = new ArrayList<NameValuePair>();
-        param.add(new BasicNameValuePair("detail","1"));
-//        param.add(new BasicNameValuePair("box", ""+mBoitierHD));
-
-        String resultat = FBMHttpConnection.getPage(FBMHttpConnection.getAuthRequestISR(url, param, true, true));
-        if (resultat != null) {
-			FBMHttpConnection.FBMLog("telechargerEtParser not null");
-    		
-	        int posChaines = resultat.indexOf("var serv_a = [");
-	        int posDisques = resultat.indexOf("var disk_a = [");
-	        
-	        if (posChaines > 0 && posDisques > 0) {
-	    		FBMHttpConnection.FBMLog("telechargerEtParser posChaines > 0 && posDisques > 0");
-	        	// Récupération du javascript correspondant à la liste des chaines
-	        	String strChaines = resultat.substring(posChaines+14, posDisques);
-	        	int finChaines = strChaines.lastIndexOf("}");
-	        	strChaines = strChaines.substring(0, finChaines+1);
-
-	        	// Conversion JSON -> objet dans la RAM
-	        	getListeChaines(strChaines);
-
-	        	// Deux boitiers HD ?
-/*	        	int posDebut = resultat.indexOf("Boitier HD");
-	        	if (posDebut > 0) {
-	        		int d, f;
-	        		String boitiers;
-	        		boitiers = resultat.substring(posDebut);
-	        		mBoitiers = new ArrayList<String>();
-	        		plusieursBoitiersHD = true;
-
-	        		do {
-	        			d = boitiers.indexOf("Boitier HD");
-	        			if (d == -1) {
-		        			break;
-	        			}
-	        			
-	        			f = d + boitiers.substring(d).indexOf("</");
-	        			mBoitiers.add(boitiers.substring(d, f));
-	        			boitiers = boitiers.substring(f);
-	        		} while (true);
-	        	}
-	        	*/
-	        	
-	        	// On met à jour le timestamp du dernier refresh
-	    		SharedPreferences mgr = activity.getSharedPreferences(KEY_PREFS, activity.MODE_PRIVATE);
-	        	Editor editor = mgr.edit();
-	        	editor.putLong(KEY_LAST_REFRESH+FBMHttpConnection.getIdentifiant(), (new Date()).getTime());
-	        	editor.commit();
-	        	return true;
+		mBoitiersName = new ArrayList<String>();
+		mBoitiersNb = new ArrayList<Integer>();
+    	do
+    	{
+	        List<NameValuePair> param = new ArrayList<NameValuePair>();
+	        param.add(new BasicNameValuePair("detail","1"));
+	        param.add(new BasicNameValuePair("box", ""+boitier));
+	
+	        String resultat = FBMHttpConnection.getPage(FBMHttpConnection.getAuthRequestISR(url, param, true, true));
+	        if (resultat != null) {
+				FBMHttpConnection.FBMLog("telechargerEtParser not null - boitier "+boitier+" / "+nbBoitiers);
+	    		
+		        int posChaines = resultat.indexOf("var serv_a = [");
+		        int posDisques = resultat.indexOf("var disk_a = [");
+		        
+		        if (posChaines > 0 && posDisques > 0) {
+		    		FBMHttpConnection.FBMLog("telechargerEtParser posChaines > 0 && posDisques > 0");
+		        	// Récupération du javascript correspondant à la liste des chaines
+		    		// On ne récupère la liste des chaînes que pour le premier boitier
+		    		if (boitier == 0)
+		    		{
+			        	String strChaines = resultat.substring(posChaines+14, posDisques);
+			        	int finChaines = strChaines.lastIndexOf("}");
+			        	strChaines = strChaines.substring(0, finChaines+1);
+			        	
+			        	// Conversion JSON -> objet
+			        	getListeChaines(strChaines);
+	
+			        	// Plusieurs boitiers HD ?
+			        	int posDebut = resultat.indexOf("box=");
+			        	if (posDebut > 0)
+			        	{
+			        		int d, f;
+			        		String boitiers;
+			        		boitiers = resultat.substring(posDebut);
+		
+			        		do {
+			        			d = boitiers.indexOf("box=");
+			        			if (d == -1) {
+				        			break;
+			        			}
+					        	boitiers = boitiers.substring(d);
+					        	f = boitiers.indexOf("\"");
+					        	FBMHttpConnection.FBMLog("Boitier parse : "+boitiers.substring(4, f));
+					        	bNum = Integer.parseInt(boitiers.substring(4, f));
+					        	mBoitiersNb.add(bNum);
+					        	d = boitiers.indexOf("Boitier HD");
+					        	boitiers = boitiers.substring(d);
+			        			
+			        			f = boitiers.indexOf("</");
+			        			String bname = boitiers.substring(0, f);
+			        			mBoitiersName.add(bname);
+			        			boitiers = boitiers.substring(f);
+			        			nbBoitiers++;
+			        			FBMHttpConnection.FBMLog("Boitier : "+bname+" n="+bNum);
+			        		} while (true);
+			        	}
+			        	else
+			        	{
+			        		mBoitiersName.add("Freebox HD");
+			        		mBoitiersNb.add(0);
+			        	}
+		    		}
+		        	// Pour chaque boitier, on récupère la liste des disques
+		        	String strDisques = resultat.substring(posDisques+14);
+		        	int fin = strDisques.lastIndexOf("}];")+1;
+		        	strDisques = strDisques.substring(0, fin);
+		        	getListeDisques(strDisques, mBoitiersName.get(boitier), mBoitiersNb.get(boitier));
+		    		FBMHttpConnection.FBMLog("Boitier courant : "+mBoitiersName.get(boitier));
+		        }
+		        else {
+		    		FBMHttpConnection.FBMLog("telechargerEtParser impossible de trouver le json dans le html");
+		    		FBMHttpConnection.FBMLog(resultat);
+		    		ok = false;
+		        }
 	        }
 	        else {
-	    		FBMHttpConnection.FBMLog("telechargerEtParser impossible de trouver le json dans le html");
-	    		FBMHttpConnection.FBMLog(resultat);
+	        	FBMHttpConnection.FBMLog("telechargerEtParser null");
+	        	ok = false;
 	        }
-        }
-        else {
-        	FBMHttpConnection.FBMLog("telechargerEtParser null");
-        }
+	        boitier++;
+    	} while (boitier < nbBoitiers);
+    	if (ok)
+    	{
+	    	// On met à jour le timestamp du dernier refresh
+			SharedPreferences mgr = activity.getSharedPreferences(KEY_PREFS, activity.MODE_PRIVATE);
+	    	Editor editor = mgr.edit();
+	    	editor.putLong(KEY_LAST_REFRESH+FBMHttpConnection.getIdentifiant(), (new Date()).getTime());
+	    	editor.commit();
+	    	return true;
+    	}
         FBMHttpConnection.FBMLog("==> Impossible de télécharger le json des chaines/disques");
     	return false;
     }
     
 	// Fonctions pour parser le javascript listant les chaines et les disques durs
 	private void getListeChaines(String strChaines) {
-		getListe(strChaines, "}]},{\"name\"", 3);
+		getListe(strChaines, "}]},{\"name\"", 3, null, 0);
 	}
 
+	private void getListeDisques(String strDisques, String bName, int bNumber) {
+		getListe(strDisques, "}", 1, bName, bNumber);
+	}
+	
 	/**
 	 * Crée la liste des chaines ou des disques (selon le boolean)
 	 * @param strSource:	l'array de JSON (commence par { sinon crash)
@@ -137,7 +181,7 @@ public class PvrNetwork extends AsyncTask<Void, Integer, Boolean> implements Con
 	 * @param shift:		le nombre d'octets inutiles entre deux objets JSON
 	 * @param isChaines:	true si c'est la liste des chaines, false si c'est celle des disques
 	 */
-	private void getListe(String strSource, String sep, int shift) {
+	private void getListe(String strSource, String sep, int shift, String bName, int bNumber) {
 		String str;
 		int pos;
 		ChainesDbAdapter db;
@@ -160,11 +204,18 @@ public class PvrNetwork extends AsyncTask<Void, Integer, Boolean> implements Con
 			// Récupération du string JSON
 			str = strSource.substring(0, pos);
 			
-			// Ajout à la liste
-			Chaine chaine = new Chaine(str);
-			chaine.storeDb(db);
-			FBMHttpConnection.FBMLog("CHAINE : "+chaine.getName());
-			
+			if (bName == null)
+			{
+				Chaine chaine = new Chaine(str);
+				chaine.storeDb(db);
+				FBMHttpConnection.FBMLog("CHAINE : "+chaine.getName());
+			}
+			else
+			{
+				Disque disque = new Disque(str);
+				disque.storeDb(db, bName, bNumber);
+				FBMHttpConnection.FBMLog("BOITIER "+bNumber+" : "+bName+" - DISQUE : "+disque.getLabel());
+			}
 			// Préparation du prochain item JSON
 			if (strSource.length() > pos+1) {
 				strSource = strSource.substring(pos+1);
@@ -173,7 +224,14 @@ public class PvrNetwork extends AsyncTask<Void, Integer, Boolean> implements Con
 				break;
 			}
 		} while(true);
-		db.swapChaines();
+		if (bName == null)
+		{
+			db.swapChaines();
+		}
+		else
+		{
+			db.swapBoitiersDisques();
+		}
 		db.close();
     }
 }
