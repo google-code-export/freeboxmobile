3da16ec7579e1054f9aa34aa6ec3064b69851bcf
olivier.rosello34 <olivier.rosello34@5c0d1bba-9ab4-11de-979f-3176e26b92dd>
2010-02-28 10:08:54 +0000

    Compte : correction d'un bug qui faisait que si on modifait le titre du compte actif, l'ancien titre continuait à apparaitre en haut des modules jusqu'à ce qu'on ait selectionné un autre compte puis qu'on soit revenu.

--- trunk/src/org/madprod/freeboxmobile/pvr/EnregistrementsActivity.java
+++ trunk/src/org/madprod/freeboxmobile/pvr/EnregistrementsActivity.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ExpandableListActivity;
 import android.app.ProgressDialog;
@@ -25,6 +26,8 @@ import android.widget.SimpleExpandableListAdapter;
 import android.widget.Toast;
 import android.widget.ExpandableListView.ExpandableListContextMenuInfo;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 import org.madprod.freeboxmobile.FBMHttpConnection;
 import org.madprod.freeboxmobile.R;
 
@@ -44,7 +47,7 @@ public class EnregistrementsActivity extends ExpandableListActivity {
     private static ListeEnregistrements listeEnregistrements = null;
     public static EnregistrementsActivity enrAct = null;
     private AsyncTask<Void, Integer, Boolean> task = null;
-
+    
 	static final int MENU_UPDATE = 0;
 	static final int MENU_ADD = 1;
 	
@@ -61,7 +64,7 @@ public class EnregistrementsActivity extends ExpandableListActivity {
 	
 	static final String DIR_PVR = "/pvr/";
 	
-	ProgressDialog progressDialog = null;
+	static ProgressDialog progressDialog = null;
 	
 	static String curId = "";
 
@@ -313,24 +316,10 @@ public class EnregistrementsActivity extends ExpandableListActivity {
 			);
 		setListAdapter(expListAdapter);
     }
-    
-    @Override
-    public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
-    	super.onChildClick(parent, v, groupPosition, childPosition, id);
-    	
-    	if (succesChargement == false) {
-    		return false;
-    	}
-    	
-    	afficherEnregistrementActivity(new Intent(this, EnregistrementActivity.class), groupPosition, ACTIVITY_ENREGISTREMENT);
-    	
-		return true;
-    }
-    
+
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
     	super.onCreateContextMenu(menu, v, menuInfo);
-    	menu.add(0, CMENU_VOIR, 0, getString(R.string.pvrCMenuVoir));
     	menu.add(0, CMENU_MODIF, 0, getString(R.string.pvrCMenuModif));
     	menu.add(0, CMENU_SUPPR, 0, getString(R.string.pvrCMenuSuppr));
     }
@@ -340,14 +329,11 @@ public class EnregistrementsActivity extends ExpandableListActivity {
     	final ExpandableListContextMenuInfo info = (ExpandableListContextMenuInfo) item.getMenuInfo();
     	int itemId = ExpandableListView.getPackedPositionGroup(info.packedPosition);
 		switch (item.getItemId()) {
-			case CMENU_VOIR:
-				afficherEnregistrementActivity(new Intent(this, EnregistrementActivity.class), itemId, ACTIVITY_ENREGISTREMENT);
-				return true;
 			case CMENU_MODIF:
 				afficherEnregistrementActivity(new Intent(this, ProgrammationActivity.class), itemId, ACTIVITY_PROGRAMMATION);
 				return true;
 			case CMENU_SUPPR:
-    			EnregistrementActivity.SupprimerEnregistrement(enrAct, false, getRowIdFromItemId(itemId));
+    			SupprimerEnregistrement(enrAct, getRowIdFromItemId(itemId));
 				return true;
 			default:
 				return super.onContextItemSelected(item);
@@ -491,6 +477,119 @@ public class EnregistrementsActivity extends ExpandableListActivity {
 				result.add(secList);
 			}
 			return result;
+		}
+	}
+	
+	private static void doSuppression(Activity activity, long rowId)
+	{
+        // Vars
+		List<NameValuePair> postVars = new ArrayList<NameValuePair>();
+		String ide, chaine_id, service_id, date, h, min, dur, name, where_id, repeat_a;
+		    			
+		// Post vars pour suppression
+    	// ide=11&chaine_id=6&service_id=0&date=31%2F12%2F2009&h=23
+    	// &min=09&dur=15&name=titre&where_id=0&repeat_a=&supp=Supprimer
+		
+		// DB
+        EnregistrementsDbAdapter db = new EnregistrementsDbAdapter(activity);
+        db.open();
+        Cursor c = db.fetchEnregistrement(rowId);
+        db.close();
+        
+        if (c == null || c.moveToFirst() == false) {
+        	return;
+        }
+
+		ide = c.getString(c.getColumnIndex(EnregistrementsDbAdapter.KEY_IDE));
+		chaine_id = c.getString(c.getColumnIndex(EnregistrementsDbAdapter.KEY_CHAINE_ID));
+		service_id = c.getString(c.getColumnIndex(EnregistrementsDbAdapter.KEY_SERVICE_ID));
+		date = c.getString(c.getColumnIndex(EnregistrementsDbAdapter.KEY_DATE));
+		h = c.getString(c.getColumnIndex(EnregistrementsDbAdapter.KEY_H));
+		min = c.getString(c.getColumnIndex(EnregistrementsDbAdapter.KEY_MIN));
+		dur = c.getString(c.getColumnIndex(EnregistrementsDbAdapter.KEY_DUR));
+		name = c.getString(c.getColumnIndex(EnregistrementsDbAdapter.KEY_NAME));
+		where_id = c.getString(c.getColumnIndex(EnregistrementsDbAdapter.KEY_WHERE_ID));
+		repeat_a = c.getString(c.getColumnIndex(EnregistrementsDbAdapter.KEY_REPEAT_A));
+		
+		c.close();
+
+		// Creation des variables POST
+		postVars.add(new BasicNameValuePair("ide", ide));
+		postVars.add(new BasicNameValuePair("chaine_id", chaine_id));
+		postVars.add(new BasicNameValuePair("service_id", service_id));
+		postVars.add(new BasicNameValuePair("date", date));
+		postVars.add(new BasicNameValuePair("h", h));
+		postVars.add(new BasicNameValuePair("min", min));
+		postVars.add(new BasicNameValuePair("dur", dur));
+		postVars.add(new BasicNameValuePair("name", name));
+		postVars.add(new BasicNameValuePair("where_id", where_id));
+		postVars.add(new BasicNameValuePair("repeat_a", repeat_a));
+
+		postVars.add(new BasicNameValuePair("supp", "Supprimer"));
+		
+		// Requete HTTP
+		String url = "http://adsl.free.fr/admin/magneto.pl";
+		FBMHttpConnection.postAuthRequest(url, postVars, true, false);
+	}
+
+	public static void SupprimerEnregistrement (final Activity activity, final long rowId)
+	{
+		AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+		builder.setMessage(activity.getString(R.string.pvrConfirmationSuppression))
+		       .setCancelable(false)
+		       .setPositiveButton(R.string.oui, new DialogInterface.OnClickListener()
+		       {
+		           public void onClick(DialogInterface dialog, int id)
+		           {
+		    			new DeleteEnregistrementTask(activity).execute(rowId);
+		           }
+		       })
+		       .setNegativeButton(R.string.non, new DialogInterface.OnClickListener()
+		       {
+		           public void onClick(DialogInterface dialog, int id) {
+		                dialog.cancel();
+		           }
+		       });
+		AlertDialog alert = builder.create();
+		alert.show();
+	}
+	
+	static class DeleteEnregistrementTask extends AsyncTask<Long, Integer, Void> {
+		Activity activity = null;
+		long rowId;
+		
+		DeleteEnregistrementTask(Activity a)
+		{
+			activity = a;
+		}
+
+		protected void onPreExecute() {
+	    	progressDialog = ProgressDialog.show(activity, activity.getString(R.string.pvrPatientez),
+	    			activity.getString(R.string.pvrSuppressionEnCours), true, false);
+		}
+
+		@Override
+		protected Void doInBackground(Long... params)
+		{
+			rowId = params[0];
+			doSuppression(activity, rowId);
+			
+			// TODO: vérifier la réponse de free, si la suppression a bien
+			// été faite!
+			
+			EnregistrementsDbAdapter db = new EnregistrementsDbAdapter(activity);
+			db.open();
+            db.deleteEnregistrement(rowId);
+            db.close();
+
+			return null;
+		}
+		
+		protected void onPostExecute(Void v)
+		{
+			progressDialog.dismiss();
+			progressDialog = null;
+			((EnregistrementsActivity) activity).updaterEnregistrements(false);
 		}
 	}
 }
