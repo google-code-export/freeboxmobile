6112d16d8b6b4434c9a88c6e6ef8aab08d91563a
olivier.rosello34 <olivier.rosello34@5c0d1bba-9ab4-11de-979f-3176e26b92dd>
2010-03-01 10:51:57 +0000

    - PVR : option de menu contextuel "Partager" dans la liste des enregistrements - GUIDE : bouton "Partager" dans les détails d'un programme - version 0.30.4

--- src/org/madprod/freeboxmobile/guide/GuideDetailsActivity.java
+++ src/org/madprod/freeboxmobile/guide/GuideDetailsActivity.java
@@ -40,11 +40,12 @@ public class GuideDetailsActivity extends Activity implements GuideConstants
         final Bundle extras = getIntent().getExtras();
         if (extras != null)
         {
-	        TextView titreEmission = (TextView) findViewById(R.id.GuideDetailsTitre);
-	        TextView dateHeureEmission = (TextView) findViewById(R.id.GuideDetailsDateHeure);
+	        final TextView titreEmission = (TextView) findViewById(R.id.GuideDetailsTitre);
+	        final TextView dateHeureEmission = (TextView) findViewById(R.id.GuideDetailsDateHeure);
 	        TextView dureeEmission = (TextView) findViewById(R.id.GuideDetailsDuree);
 	        TextView descEmission = (TextView) findViewById(R.id.GuideDetailsDesc);
 	        TextView nomChaine = (TextView) findViewById(R.id.GuideDetailsNomChaine);
+	        Button partager = (Button)  findViewById(R.id.GuideDetailsButtonPartager);
 	        Button enregistrer = (Button) findViewById(R.id.GuideDetailsButtonEnregistrer);
 	        Button regarder = (Button) findViewById(R.id.GuideDetailsButtonRegarder);
 	        ImageView logoChaine = (ImageView) findViewById(R.id.GuideDetailsLogoChaine);
@@ -59,6 +60,24 @@ public class GuideDetailsActivity extends Activity implements GuideConstants
 	        String amj[] = date[0].split("-");
 	        String hm[] = date[1].split(":");
 	        dateHeureEmission.setText("Diffusé le "+amj[2]+"/"+amj[1]+"/"+amj[0]+" à "+hm[0]+"h"+hm[1]);
+
+	        partager.setOnClickListener(
+					new View.OnClickListener()
+					{
+						@Override
+						public void onClick(View arg0)
+						{
+							String text = titreEmission.getText()+"\n"+dateHeureEmission.getText()+"\n\n"+"Partagé par FreeboxMobile pour Android.";
+					        Intent i = new Intent(Intent.ACTION_SEND)
+				    		.putExtra(Intent.EXTRA_TEXT, text).setType("text/plain")
+				    		.putExtra(Intent.EXTRA_SUBJECT, "ProgrammeTV partagé par FreeboxMobile")
+//				    		.addCategory(Intent.CATEGORY_DEFAULT)
+				    		;
+					    	startActivityForResult(Intent.createChooser(i, "Partagez ce programme avec"),0);
+						}
+					}
+				);
+
 
 	        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	        try
@@ -93,7 +112,7 @@ public class GuideDetailsActivity extends Activity implements GuideConstants
 			}
 	        nomChaine.setText(extras.getString(ChainesDbAdapter.KEY_GUIDECHAINE_NAME)+" (canal "+
 	        		((Integer)extras.getInt(ChainesDbAdapter.KEY_GUIDECHAINE_CANAL)).toString()+")");
-	        regarder.setText("Regarder "+extras.getString(ChainesDbAdapter.KEY_GUIDECHAINE_NAME));
+	        regarder.setText("Regarder "/*+extras.getString(ChainesDbAdapter.KEY_GUIDECHAINE_NAME)*/);
 	        String filepath = Environment.getExternalStorageDirectory().toString()+DIR_FBM+DIR_CHAINES+extras.getString(ChainesDbAdapter.KEY_GUIDECHAINE_IMAGE);
 			Bitmap bmp = BitmapFactory.decodeFile(filepath);
 			if (bmp != null)
