e49625f111fc50e8fbcaf1280dd4d862b456b07c
olivier.rosello34 <olivier.rosello34@5c0d1bba-9ab4-11de-979f-3176e26b92dd>
2010-12-16 10:08:46 +0000

    first release of tv module

---
+++ Tv/src/org/madprod/freeboxmobile/tv/TvConstants.java
@@ -0,0 +1,23 @@
+package org.madprod.freeboxmobile.tv;
+
+/**
+*
+* @author Olivier Rosello
+* $Id$
+* 
+*/
+
+public interface TvConstants
+{
+	static final String M_STREAMURL = "http://tv.freebox.fr/";
+	static final String M_LOGOURL = "http://tv.freebox.fr/images/";
+	static final String M_LOGO = "logo";
+    static final String M_TITRE = "titre";
+    static final String M_SHORT = "short";
+    static final String M_ID = "id";
+    static final String KEY_SPLASH_TV = "splashscreen_mevo";
+    
+    static final String ANALYTICS_MAIN_TRACKER = "UA-9016955-4";
+    static final String KEY_PREFS		= "freeboxmobiletv";
+    static final String TAG				= "FBMTV";
+}
