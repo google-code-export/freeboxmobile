5f7bf0e58fd7018824013b193ece6c4b89bf24af
clement.beslon <clement.beslon@5c0d1bba-9ab4-11de-979f-3176e26b92dd>
2011-01-09 17:50:41 +0000

    add functionality of apps2sd

--- MainApp/src/org/madprod/freeboxmobile/OnExternalApp.java
+++ MainApp/src/org/madprod/freeboxmobile/OnExternalApp.java
@@ -1,38 +1,35 @@
 package org.madprod.freeboxmobile;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
 import org.madprod.freeboxmobile.guide.GuideCheck; 
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.sax.StartElementListener;
 import android.util.Log;
 
 /**
-*
-* @author Olivier Rosello
-* $Id$
-* 
-*/
+ *
+ * @author Olivier Rosello
+ * $Id$
+ * 
+ */
 
 public class OnExternalApp extends BroadcastReceiver implements Constants
 {
+	String[] packages = new String[]{"org.madprod.infofreenautes", "org.madprod.mevo"};
+
 	@Override
 	public void onReceive(Context context, Intent intent)
 	{
-		Log.d(TAG, "=====================> ON EXTERNAL APP !"+intent.toString());
-		
-		String ms = context.getSharedPreferences(KEY_PREFS, Context.MODE_PRIVATE).getString(KEY_MEVO_PREFS_FREQ, "-1");
-		if (!ms.equals("0")) // Si "0" : l'utilisateur ne veut pas de relève périodique
-		{
-			if (!ms.equals("-1"))  // Si une valeur était mise
-			{
-//				MevoSync.changeTimer(Integer.parseInt(ms), context);
-			}
-			else // Si pas configuré : valeur par défaut
-			{
-//				MevoSync.changeTimer(DEFAULT_MEVO_FREQ, context);
-			}
-		}
+		Log.d(TAG, "=====================> ON EXTERNAL APP !"+intent.toString());		
+		Intent i = new Intent("org.madprod.freeboxmobile.action.MOUNTMODULE");
+		context.sendBroadcast(i);
+
 
 		GuideCheck.setTimer(context);
 	}
